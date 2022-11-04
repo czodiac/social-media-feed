8c112a0c0c17aa1ee7fcf181f76482002ba565fb
ITL Studio <iltae80@daum.net>
2022-11-03 21:19:48 -0600

    Loading Users from MSW

--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,15 +1,23 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { client } from '../../api/client'
 
-const initialState = [
-    { id: '0', name: 'Tana' },
-    { id: '1', name: 'Iltae' },
-    { id: '2', name: 'Lym' }
-]
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+    const response = await client.get('/fakeApi/users')
+    return response.data
+})
 
 const usersSlice = createSlice({
     name: 'users',
     initialState,
-    reducers: {}
+    reducers: {},
+    extraReducers(builder) {
+        builder.addCase(fetchUsers.fulfilled, (state, action) => {
+            // We're returning the action.payload directly. Immer lets us update state in two ways: either mutating the existing state value, or returning a new result. If we return a new value, that will replace the existing state completely with whatever we return. 
+            return action.payload
+        })
+    }
 })
 
 export default usersSlice.reducer
