ccc3265fb512e0cc0ab6a0c3520530a555044a43
ITL Studio <iltae80@daum.net>
2022-11-03 18:46:55 -0600

    Loading State for Requests
    Don't use hard-coded posts data. Introduce loading state/error to initialState.

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,23 +2,11 @@
 import { createSlice, nanoid } from '@reduxjs/toolkit' // nanoid: // To generated a random unique ID
 import { sub } from 'date-fns';
 
-const initialState = [
-    {
-        id: '1',
-        title: 'First post',
-        content: 'Hi',
-        date: sub(new Date(), { minutes: 5 }).toISOString(),
-        reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 }
-    },
-    {
-        id: '2',
-        title: 'Second',
-        content: 'Text',
-        date: sub(new Date(), { minutes: 10 }).toISOString(),
-        reactions: { thumbsUp: 0, hooray: 0, heart: 0, rocket: 0, eyes: 0 }
-
-    }
-]
+const initialState = {
+    posts: [],
+    status: 'idle',
+    error: null
+}
 
 // Our posts slice is responsible for handling all updates to the posts data
 const postsSlice = createSlice({
@@ -29,7 +17,7 @@ const postsSlice = createSlice({
         // We can export that action creator and use it in our UI components to dispatch the action when the user clicks "Save Post".
         postAdded: {
             reducer(state, action) {
-                state.push(action.payload);
+                state.posts.push(action.payload);
             },
             prepare(title, content, userId) {
                 // Unique IDs and other random values(id, date) should be put in the action, not calculated in the reducer
@@ -47,7 +35,7 @@ const postsSlice = createSlice({
         },
         postUpdated(state, action) {
             const { id, title, content } = action.payload
-            const existingPost = state.find(post => post.id === id)
+            const existingPost = state.posts.find(post => post.id === id)
             if (existingPost) {
                 existingPost.title = title
                 existingPost.content = content
@@ -57,7 +45,7 @@ const postsSlice = createSlice({
         reactionAdded(state, action) {
             const { postId, reaction } = action.payload;
             // It's always better to keep the action objects as small as possible, and do the state update calculations in the reducer.
-            const existingPost = state.find(post => post.id === postId)
+            const existingPost = state.posts.find(post => post.id === postId)
             if (existingPost) {
                 existingPost.reactions[reaction]++
             }
@@ -67,5 +55,5 @@ const postsSlice = createSlice({
 
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 export default postsSlice.reducer
-export const selectAllPosts = state => state.posts;
-export const selectPostById = (state, postId) => state.posts.find(post => post.id === postId)
+export const selectAllPosts = state => state.posts.posts;
+export const selectPostById = (state, postId) => state.posts.posts.find(post => post.id === postId)
